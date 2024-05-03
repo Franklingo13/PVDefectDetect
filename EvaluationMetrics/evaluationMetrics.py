fb88dfc2d36efe64da4b5d4a2d7339cbfc073ba8
Franklin Gómez <franklingo13@gmail.com>
2024-05-03 13:25:40 -0500

    Se crea fichero para almacenar las funciones para las métricas de evaluación

---
+++ EvaluationMetrics/evaluationMetrics.py
@@ -0,0 +1,78 @@
+# Importar librerías
+import os
+import numpy as np
+import cv2
+
+# Fichero para almacenar los diferentes métodos de evaluación de algoritmos
+
+# Función para medir la métrica Contrast en imágenes en escala de grises
+def contrast_metric(image):
+    # Calcular la intensidad media de la imagen
+    E = np.mean(image)
+
+    # Obtener el número de niveles de grises (L)
+    num_gray_levels = np.max(image) + 1
+
+    # Calcular la probabilidad de ocurrencia de cada valor de píxel
+    hist, _ = np.histogram(image, bins=num_gray_levels, range=(0, num_gray_levels))
+    P = hist / float(np.sum(hist))
+
+    # Calcular el contraste
+    C = np.sqrt(np.sum([(j - E)**2 * P[j] for j in range(num_gray_levels)]))
+
+    return C
+
+# Función para medir la métrica contrast Improvement ratio (CIR) en imágenes en escala de grises
+def CIR(original_image, enhanced_image):
+    # Calcular el tamaño de la imagen
+    M, N = original_image.shape
+
+    # Inicializar las sumas para el numerador y el denominador
+    numerator_sum = 0
+    denominator_sum = 0
+
+    # Calcular el CIR
+    for u in range(1, M-1):
+        for v in range(1, N-1):
+            # Calcular el contraste local para la imagen original
+            w_original = abs(original_image[u, v] - np.mean(original_image[u-1:u+2, v-1:v+2])) / (original_image[u, v] + np.mean(original_image[u-1:u+2, v-1:v+2]))
+            
+            # Calcular el contraste local para la imagen mejorada
+            w_enhanced = abs(enhanced_image[u, v] - np.mean(enhanced_image[u-1:u+2, v-1:v+2])) / (enhanced_image[u, v] + np.mean(enhanced_image[u-1:u+2, v-1:v+2]))
+
+            # Actualizar las sumas para el numerador y el denominador
+            numerator_sum += (w_original - w_enhanced)**2
+            denominator_sum += w_original**2
+
+    # Calcular el CIR
+    CIR = numerator_sum / denominator_sum
+
+    return CIR
+
+# Función para calcular el PSNR entre las imágenes original y mejorada
+def PSNR(original_image, enhanced_image):
+    # Calcular el error cuadrático medio
+    mse = np.mean((original_image - enhanced_image)**2)
+    print("max_pixel", np.max(original_image))
+
+    # Calcular el PSNR
+    psnr = 10 * np.log10((255**2) / mse)
+
+    return psnr
+
+# Función para calcular el ínidice de borrosidad en imágenes en escala de grises
+def blur_metric(enhanced_image):
+    # Calcular el índice de borrosidad
+    M, N = enhanced_image.shape
+    p_xy = np.sin(np.pi / 2 * (1 - enhanced_image / np.max(enhanced_image)))
+    blur_index = (2 / (M * N)) * np.sum(np.minimum(p_xy, (1 - p_xy)))
+    return blur_index
+
+# Función para calcular PL metric en imágenes en escala de grises
+def PL(original_image, enhanced_image):
+    psnr = PSNR(original_image, enhanced_image)
+    blur = blur_metric(enhanced_image)
+
+    # Calcular PL
+    PL = psnr / blur
+    return PL
