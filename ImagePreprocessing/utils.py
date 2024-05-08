389c7b5f9d33444f368b7c522562c91fb512eb2d
Franklin Gómez <franklingo13@gmail.com>
2024-05-08 13:33:44 -0500

    Se agrega una función para mostrar una imagen con ROI `mostrar_imagen_con_roi(imagen, roi, titulo, esquina_superior_izquierda, ancho, alto)`

--- ImagePreprocessing/utils.py
+++ ImagePreprocessing/utils.py
@@ -5,6 +5,8 @@ import os
 from os import listdir, path
 import numpy as np
 import cv2
+import matplotlib.patches as patches
+from matplotlib import pyplot as plt
 
 # Función para la lectura de imágenes
 def read_images(directory, allow_color_images=False):
@@ -32,6 +34,41 @@ def read_images(directory, allow_color_images=False):
                 image = cv2.imread(path.join(directory, f), cv2.IMREAD_COLOR)
             images.append(image)
     return images
+
+def mostrar_imagen_con_roi(imagen, roi, titulo, esquina_superior_izquierda, ancho, alto):
+    """
+    Muestra una imagen con una zona ampliada o ROI.
+
+    Parámetros:
+        imagen: La imagen a mostrar.
+        roi: La zona ampliada (ROI).
+        titulo: El título de la ventana.
+        esquina_superior_izquierda: Una tupla (x, y) que representa las coordenadas de la esquina superior izquierda de la ROI.
+        ancho: El ancho de la ROI.
+        alto: El alto de la ROI.
+    """
+    # Crear una figura de Matplotlib
+    fig, axes = plt.subplots(1, 2, figsize=(10, 5))
+
+    # Mostrar la imagen original en el primer eje
+    axes[0].imshow(imagen, cmap='gray')
+    axes[0].set_title(titulo)
+    axes[0].axis('off')
+
+    # Dibujar un rectángulo alrededor de la ROI en la imagen original
+    rect = patches.Rectangle(esquina_superior_izquierda, ancho, alto, linewidth=1, edgecolor='r', facecolor='none')
+    axes[0].add_patch(rect)
+
+    # Mostrar la zona ampliada (ROI) en el segundo eje
+    axes[1].imshow(roi, cmap='gray')
+    axes[1].set_title('Zona ampliada')
+    axes[1].axis('off')
+
+    # Ajustar el layout de la figura
+    plt.tight_layout()
+
+    # Mostrar la figura
+    plt.show()
 
 # Función para guardar una imagen en un directorio
 def save_image(image, directory, filename):
