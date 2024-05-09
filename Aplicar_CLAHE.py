1c39d230cfb100b8fbfd2388d20c61b1c8c3c40b
Franklin Gómez <franklingo13@gmail.com>
2024-05-09 11:35:46 -0500

    Aplicar algoritmo CLAHE a un dataset y guardar imágenes mejoradas

---
+++ Aplicar_CLAHE.py
@@ -0,0 +1,28 @@
+import os
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+from ImagePreprocessing.utils import *
+from ImagePreprocessing.contrast_enhancement import *
+from EvaluationMetrics.evaluationMetrics import *
+
+## Aplicación del algoritmo CLAHE a un dataset,  y guardado de las imágenes mejoradas
+# en este caso: datasetNoBG  
+salidas_path = read_folder_path(r"D:\Documentos\Universidad de Cuenca\Trabajo de Titulación\PVDefectDetect\ImagePreprocessing\Salidas\TestContraste")
+dataset_path = read_folder_path(r"D:\Documentos\Universidad de Cuenca\Trabajo de Titulación\PVDefectDetect\ImagePreprocessing\Salidas\TestContraste\datasetNoBG")
+
+dataset = read_images(dataset_path)
+
+datasetNoBG_CLAHE = []
+
+for i, image in enumerate(dataset):
+    datasetNoBG_CLAHE.append(CLAHE(image))
+
+
+## Creación de un dataset con las imágenes mejoradas, que se almacena en el directorio `salidas_path/datasetCLAHE`
+os.makedirs(os.path.join(salidas_path, "datasetNoBG_CLAHE"), exist_ok=True)
+for i, image in enumerate(datasetNoBG_CLAHE):
+    cv2.imwrite(os.path.join(salidas_path, "datasetNoBG_CLAHE", f"NoBGCLAHE_{i}.jpg"), image)
+print("Imágenes guardadas en datasetNoBG_CLAHE")
+print("Número de imágenes en datasetNoBG_CLAHE:", len(datasetNoBG_CLAHE))
+
