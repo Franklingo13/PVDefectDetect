3fbf17e85cf43ca6caacf0edbecbe4dd41c0c1e5
Franklin Gómez <franklingo13@gmail.com>
2024-06-06 15:03:37 -0500

    Actualizar directorio del dataset a tratar, también se agrega líneas para aplicar CLAHE en el datasetNoBG

--- SubstractBG.py
+++ SubstractBG.py
@@ -8,9 +8,9 @@ from EvaluationMetrics.evaluationMetrics import *
 
 ## Aplicación del algoritmo subsctractBG a un dataset,  y guardado de las imágenes mejoradas
 
-path = r"E:\Panel_260W\V44.5_I9.16_t\JPEG"  
-bg_path = r"E:\Panel_260W\V44.5_I9.16_t\fondo_jpeg"
-salidas_path = read_folder_path(r"D:\Documentos\Universidad de Cuenca\Trabajo de Titulación\PVDefectDetect\ImagePreprocessing\Salidas\panel_260W")
+path = r"E:\Panel_260W\V40_I4.5_t\JPEG"  
+bg_path = r"E:\Panel_260W\V40_I4.5_t\fondo_jpeg"
+salidas_path = read_folder_path(r"E:\Panel_260W")
 dataset_path = read_folder_path(path)
 BG_dataset_path = read_folder_path(bg_path)
 print("Directorio imagenes: ", dataset_path)
@@ -27,8 +27,20 @@ for i in range(len(dataset)):
     datasetNoBG.append(imageNoBG)
 
 ## Creación de un dataset con las imágenes mejoradas, que se almacena en el directorio `salidas_path/nombre_carpeta`
-nombre_carpeta = "V44.5_I9.16_t_NoBG"
+nombre_carpeta = "V40_I4.5_t_NoBG"
 os.makedirs(os.path.join(salidas_path, nombre_carpeta), exist_ok=True)
 for i in range(len(datasetNoBG)):
     cv2.imwrite(os.path.join(salidas_path, nombre_carpeta, "imagen"+str(i)+".jpg"), datasetNoBG[i])
 print("Imágenes guardadas en ", nombre_carpeta)
+
+# Aplicar el algoritmo CLAHE al dataset sin fondo
+dataset_CLAHE = []
+for i, image in enumerate(datasetNoBG):
+    dataset_CLAHE.append(CLAHE(image))
+
+nombre_carpeta = "V40_I4.5_t_NoBG_CLAHE"
+os.makedirs(os.path.join(salidas_path, nombre_carpeta), exist_ok=True)
+for i, image in enumerate(dataset_CLAHE):
+    cv2.imwrite(os.path.join(salidas_path, nombre_carpeta, "imagen"+str(i)+".jpg"), image)
+print("Imágenes guardadas en ", nombre_carpeta)
+print("Número de imágenes: ", len(dataset_CLAHE))
