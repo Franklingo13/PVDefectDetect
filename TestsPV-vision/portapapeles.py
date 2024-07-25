fc999f4980dbce55f89bafd2c5c2231316bdceb3
Franklin Gómez <franklingo13@gmail.com>
2024-07-25 15:07:00 -0500

    Se actualiza el portapapeles

--- TestsPV-vision/portapapeles.py
+++ TestsPV-vision/portapapeles.py
@@ -1,5 +1,4 @@
 # Importar librerías
-
 import torch 
 from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms
@@ -27,105 +26,30 @@ import os
 # Importar Model Handler
 from pv_vision.nn import ModelHandler
 
-
-## Funciones 
-
-# Función para convertir las anotaciones de validación en máscaras booleanas 
-# de la misma manera que `get_masks` procesa las predicciones del modelo.
-def get_annotation_masks(annotations):
-    masks_each = []
-    for annotation in annotations:
-        annotation = cv.resize(annotation, (256, 256))  # Redimensionar anotación al tamaño de las predicciones
-        busbar = (annotation == 4) #4
-        crack = (annotation == 3) #3
-        cross = (annotation == 2) #2
-        dark = (annotation == 1)  #1
-        # cross = (annotation == 1)
-        # busbar = (annotation == 2)
-        # crack = (annotation == 3)
-        # dark = (annotation == 4)
-        # Convertir las máscaras de NumPy a tensores de PyTorch y apilarlas.
-        masks_tensor = torch.stack([torch.from_numpy(busbar.astype(np.float32)), 
-                                    torch.from_numpy(crack.astype(np.float32)), 
-                                    torch.from_numpy(cross.astype(np.float32)), 
-                                    torch.from_numpy(dark.astype(np.float32))])
-        #masks_each.append(masks_tensor)
-        masks_each.append(torch.dstack([busbar, crack, cross, dark]).permute(2, 0, 1))
-        #masks_each.append(torch.dstack([busbar, crack, cross, dark]).permute(2, 0, 1))
-    return torch.stack(masks_each)
-
-def show_comparison(pred_mask, true_mask, class_names):
-    num_classes = len(class_names)
-    fig, axes = plt.subplots(num_classes, 2, figsize=(10, num_classes * 5))
-    
-    for i, class_name in enumerate(class_names):
-        # Predicted mask for the class
-        axes[i, 0].imshow(pred_mask[i], cmap='gray')
-        axes[i, 0].set_title(f'Predicted Mask - {class_name}')
-        axes[i, 0].axis('off')
-        
-        # True mask for the class
-        axes[i, 1].imshow(true_mask[i], cmap='gray')
-        axes[i, 1].set_title(f'True Mask - {class_name}')
-        axes[i, 1].axis('off')
-    
-    plt.tight_layout()
-    plt.show()
-
-# Definir la transformación de las imágenes que se pasará al manejador del modelo
-transform = transforms.Compose([
-    # Convertir la imagen a un tensor de PyTorch y escalar los valores de los píxeles entre 0 y 1
-    transforms.ToTensor(),
-    # Normalizar cada canal de color de la imagen. Los valores de la media y la desviación estándar se especifican para cada canal (RGB). 
-    # Estos valores son los valores de media y desviación estándar del conjunto de datos ImageNet.
-    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
-    ])
-
-# Definir una clase personalizada que hereda de Dataset
+# ### Predicción con un modelo ya entrenado
 class myDataset(Dataset):
     # El método de inicialización se llama cuando se crea una instancia de la clase
     def __init__(self, images, transform):
         # Guardar las imágenes y la transformación como atributos de la instancia
-        self.images = images
-        self.transform = transform
-
-    # El método __len__ devuelve el número de elementos en el conjunto de datos
-    def __len__(self):
-        return len(self.images)
-
-    # El método __getitem__ se utiliza para obtener un elemento del conjunto de datos
-    def __getitem__(self, idx):
-        # Redimensionar la imagen al tamaño deseado
-        image = cv.resize(self.images[idx], (256, 256))
-        # Aplicar la transformación a la imagen
-        image = self.transform(image)
-        
+        .....
         # Devolver la imagen transformada
         return image
-
 # Carpeta de imágenes
 val_imgs_path = 'D:/Documentos/Universidad de Cuenca/Trabajo de Titulación/CellAnotation_no_humanMasks/dataset_cells/images'
 #val_imgs_path = 'D:/Documentos/PV_Vision/crack_segmentation/crack_segmentation/val/img'
-
 # Carpeta de anotaciones
 val_annotations_path = 'D:/Documentos/Universidad de Cuenca/Trabajo de Titulación/CellAnotation_no_humanMasks/dataset_cells/annotations'
 #val_annotations_path = 'D:/Documentos/PV_Vision/crack_segmentation/crack_segmentation/val/ann'
 images_val = [cv.imread(file) for file in list_images(val_imgs_path)]           #imágenes de validación
 ann_val = [cv.imread(file) for file in list_images(val_annotations_path)]       # anotaciones de validación
-# Verificar que las imágenes se cargaron correctamente
-print(f'Número de imágenes de validación: {len(images_val)}')
-print(f'Número de anotaciones de validación: {len(ann_val)}')
-
 # Crear el dataset
 imgset = myDataset(images_val, transform)
-
 # Definir el dispositivo y cargar el modelo
 weight_path = 'D:/Documentos/PV_Vision/Neural_Network_W/crack_segmentation/unet_oversample_low_final_model_for_paper/model.pt'
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 print(f'Dispositivo: {device}')
 unet = construct_unet(5)
 unet = DataParallel(unet)
-
 # Inicializar el manejador del modelo
 modelhandler = ModelHandler(
     model=unet,
@@ -136,125 +60,102 @@ modelhandler = ModelHandler(
     save_dir='D:/Documentos/Universidad de Cuenca/Trabajo de Titulación/Predicciones/Modulos/Celdas/output',
     save_name='unet_cell_prediction'
 )
-
 # Cargar los pesos del modelo y generar las predicciones
 modelhandler.load_model(weight_path)
 masks = modelhandler.predict(save=True)
 
-# Definir la transformación de redimensionamiento
-resize = transforms.Compose([transforms.Resize((256, 256)), transforms.PILToTensor()])
 
-# Definir un mapa de colores para las diferentes clases de máscaras
-color_map = {
-    'dark': (68, 114, 148),
-    'cross': (77, 137, 99),
-    'crack': (165, 59, 63),
-    'busbar': (222, 156, 83)
-}
+### Entrenamiento de un modelo nuevo
+class SolarDataset(VisionDataset):
+    """Un conjunto de datos que lee directamente las imágenes y las máscaras desde una carpeta."""
+    ....
+# Definición de una clase para componer varias transformaciones.
+class Compose:
+    def __init__(self, transforms):
+        """
+        transforms: una lista de transformaciones
+        """
+        self.transforms = transforms
 
-# Se definió una función para obtener las máscaras de las predicciones del modelo.
-def get_masks(masks_raw):
-    # Se creó una lista vacía para almacenar las máscaras.
-    masks_each = []
-    # Se aplicó la función softmax a las predicciones del modelo y se obtuvo la clase con la mayor probabilidad para cada píxel.
-    masks_all = torch.nn.functional.softmax(torch.from_numpy(masks_raw), dim=1).argmax(dim=1)
-    # Para cada máscara en masks_all, se crearon máscaras booleanas para cada clase y se añadieron a la lista masks_each.
-    for masks in masks_all:
-        busbar = masks==1
-        crack = masks==2
-        cross = masks==3
-        dark = masks==4
-        masks_each.append(torch.dstack([busbar, crack, cross, dark]).permute(2, 0, 1))
-    return masks_each
+    # Se definió el método para aplicar las transformaciones a la imagen y la máscara.
+    def __call__(self, image, target):
+        """
+        image: imagen de entrada
+        target: máscara de entrada
+        """
+        for t in self.transforms:
+            image, target = t(image, target)
+        return image, target
+# Se definió una clase para redimensionar la imagen y la máscara a un tamaño fijo.
+class FixResize:
+    # UNet requiere que el tamaño de entrada sea múltiplo de 16
+    def __init__(self, size):
+        self.size = size
 
-# Se definió una función para dibujar las máscaras sobre las imágenes.
-def draw_mask(img, masks, colors=color_map, alpha=0.6):
-    # Se convirtió la imagen a un objeto de la clase Image de PIL y se redimensionó.
-    img = Image.fromarray(img)
-    img = resize(img)
-    # Se dibujaron las máscaras sobre la imagen con la opacidad especificada y se devolvió la imagen resultante.
-    combo = draw_segmentation_masks(img, masks, alpha=alpha, colors=[colors[key] for key in ['busbar', 'crack', 'cross', 'dark']])
-    return F.to_pil_image(combo)
+    # Se definió el método para redimensionar la imagen y la máscara.
+    def __call__(self, image, target):
+        image = F.resize(image, (self.size, self.size), interpolation=transforms.InterpolationMode.BILINEAR)
+        target = F.resize(target, (self.size, self.size), interpolation=transforms.InterpolationMode.NEAREST)
+        return image, target
 
-# Obtener las máscaras predichas
-masks_each = get_masks(masks)
+# Se definió una clase para transformar la imagen y la máscara a tensores.
+class ToTensor:
+    """Transforma la imagen a tensor. Escala la imagen a [0,1] float32.
+    Transforma la máscara a tensor.
+    """
+# Se definió una clase para transformar la imagen a tensor manteniendo el tipo original.
+class PILToTensor:
+    """Transforma la imagen a tensor. Mantiene el tipo original."""
 
-# Convertir las anotaciones a tensores de imágenes para ser usadas por la función get_annotation_masks
-ann_val = [cv.imread(img_path, cv.IMREAD_GRAYSCALE) for img_path in list_images(val_annotations_path)]
+# Se definió una clase para normalizar la imagen.
+class Normalize:
+    ....
+# Ruta al directorio que contiene las imágenes y las máscaras.
+root = Path(
+    '/content/drive/MyDrive/Trabajo de titulación/PV_vision/Entrenamiento')
 
-# Obtener las máscaras de las anotaciones de validación
-true_masks = get_annotation_masks(ann_val)
+# Se definen las transformaciones a aplicar a las imágenes y las etiquetas.
+transformers = Compose([FixResize(256), ToTensor(), Normalize()])
+# Se crean los conjuntos de datos de entrenamiento, validación y prueba.
+trainset = SolarDataset(root, image_folder="train/img",
+        mask_folder="train/ann", transforms=transformers)
 
-# Comparar una máscara predicha y una máscara real
-pred_mask = masks_each[0]
-true_mask = true_masks[0]
-class_names = ['busbar', 'crack', 'cross', 'dark']
-show_comparison(pred_mask, true_mask, class_names)
+valset = SolarDataset(root, image_folder="val_resize/img",
+        mask_folder="val_resize/ann", transforms=transformers)
 
-
-# Calcular el IoU para una clase específica
-def calculate_iou(pred_mask, true_mask):
-    intersection = torch.logical_and(pred_mask, true_mask).sum().item()
-    union = torch.logical_or(pred_mask, true_mask).sum().item()
-    if union == 0:
-        return float('nan')  # Si no hay ningún píxel verdadero o predicho en esta clase, devolver NaN
-    return intersection / union
-
-# Función para calcular Precision
-def calculate_precision(pred_mask, true_mask):
-    true_positive = torch.logical_and(pred_mask, true_mask).sum().item()
-    predicted_positive = pred_mask.sum().item()
-    if predicted_positive == 0:
-        return float('nan')  # Si no hay ningún píxel predicho positivo, devolver NaN
-    return true_positive / predicted_positive
-
-# Función para calcular Recall
-def calculate_recall(pred_mask, true_mask):
-    true_positive = torch.logical_and(pred_mask, true_mask).sum().item()
-    actual_positive = true_mask.sum().item()
-    if actual_positive == 0:
-        return float('nan')  # Si no hay ningún píxel verdadero positivo, devolver NaN
-    return true_positive / actual_positive
-
-# Función para calcular F1 Score
-def calculate_f1_score(precision, recall):
-    if np.isnan(precision) or np.isnan(recall) or (precision + recall) == 0:
-        return float('nan')  # Si alguna de las métricas es NaN o la suma es 0, devolver NaN
-    return 2 * (precision * recall) / (precision + recall)
-
-# Función para evaluar las predicciones y calcular todas las métricas
-def evaluate_predictions(pred_masks, true_masks):
-    ious = { 'busbar': [], 'crack': [], 'cross': [], 'dark': [] }
-    precisions = { 'busbar': [], 'crack': [], 'cross': [], 'dark': [] }
-    recalls = { 'busbar': [], 'crack': [], 'cross': [], 'dark': [] }
-    f1_scores = { 'busbar': [], 'crack': [], 'cross': [], 'dark': [] }
-    
-    for pred_mask, true_mask in zip(pred_masks, true_masks):
-        for i, class_name in enumerate(['busbar', 'crack', 'cross', 'dark']):
-            iou = calculate_iou(pred_mask[i], true_mask[i])
-            precision = calculate_precision(pred_mask[i], true_mask[i])
-            recall = calculate_recall(pred_mask[i], true_mask[i])
-            f1_score = calculate_f1_score(precision, recall)
-            
-            ious[class_name].append(iou)
-            precisions[class_name].append(precision)
-            recalls[class_name].append(recall)
-            f1_scores[class_name].append(f1_score)
-    
-    metrics = {}
-    for class_name in ious:
-        metrics[class_name] = {
-            'IoU': np.nanmean(ious[class_name]),
-            'Precision': np.nanmean(precisions[class_name]),
-            'Recall': np.nanmean(recalls[class_name]),
-            'F1 Score': np.nanmean(f1_scores[class_name])
-        }
-    return metrics
-
-# Supongamos que tienes una predicción y una máscara verdadera para comparar:
-pred_mask = masks_each[100]  # Primera predicción del modelo
-true_mask = true_masks[100]  # Primera máscara de anotación
-# Nombres de las clases
-class_names = ['busbar', 'crack', 'cross', 'dark']
-# Mostrar la comparación
-show_comparison(pred_mask, true_mask, class_names)
+testset = SolarDataset(root, image_folder="test/img",
+        mask_folder="test/ann", transforms=transformers)
+unet = construct_unet(5)
+unet = DataParallel(unet)
+# Se define el dispositivo en el que se ejecutará el modelo.
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+criterion = CrossEntropyLoss()
+optimizer = Adam(unet.parameters(), lr=0.01)
+lr_scheduler = StepLR(optimizer, step_size=5, gamma=0.2)
+# Se inicializa el manejador del modelo.
+modelhandler = ModelHandler(
+    # Se pasa el modelo que se va a entrenar.
+    model = unet,
+    # Se pasan los conjuntos de datos de entrenamiento, validación y prueba.
+    train_dataset=trainset,
+    val_dataset=valset,
+    test_dataset=testset,
+    # Se especifica el tamaño del lote para el entrenamiento y la validación.
+    batch_size_train=16,
+    batch_size_val=16,
+    # Se pasa el programador de la tasa de aprendizaje.
+    lr_scheduler=lr_scheduler,
+    # Se especifica el número de épocas para el entrenamiento.
+    num_epochs=20,
+    # Se pasa la función de pérdida y el optimizador.
+    criterion=criterion,
+    optimizer=optimizer,
+    # Se pasa el dispositivo en el que se ejecutará el entrenamiento.
+    device=device,
+    # Se especifica el directorio donde se guardarán los puntos de control del modelo.
+    save_dir='/content/drive/MyDrive/Trabajo de titulación/PV_vision/Entrenamiento/PuntosControl/checkpoints',
+    # Se especifica el nombre del archivo de punto de control.
+    save_name='unetv5.pt'
+)
+# Se inicializa el entrenamiento del modelo.
+modelhandler.train_model()
