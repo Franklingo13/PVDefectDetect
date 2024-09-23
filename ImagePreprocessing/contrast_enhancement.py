fb367d89eba848f552d9a54c2ce8d170e4db397a
Franklin Gómez <franklingo13@gmail.com>
2024-09-23 17:22:44 -0500

    Agrega funciones para eliminar artefactos y mejorar el contraste en imágenes EL y se actualiza la descripción de funciones.

--- ImagePreprocessing/contrast_enhancement.py
+++ ImagePreprocessing/contrast_enhancement.py
@@ -1,6 +1,13 @@
-# Importación de librerías
+"""
+Este archivo contiene funciones para la eliminación de artefactos y mejora de contraste en imágenes EL.
+
+Parte del código emplea algoritmos del repositorio imgProcessor:
+https://github.com/radjkarl/imgProcessor
+"""
+
 import numpy as np
 import cv2
+from scipy.ndimage import median_filter
 
 # Fichero para almacenar los diferentes métodos de mejora de contraste
 
@@ -8,14 +15,14 @@ import cv2
 def MMC(image, n_iterations):
     """
     Función que implementa el algoritmo de Mejora de Contraste por Morfología
-    de Contraste Máximo (MMCE)
+    de Contraste Máximo (MMCE).
 
-    Parámetros:
-    image: Imagen de entrada en escala de grises.
-    n_iterations: Número de iteraciones a realizar.
+    Args:
+        image (numpy.ndarray): Imagen de entrada en escala de grises.
+        n_iterations (int): Número de iteraciones a realizar.
 
-    Retorna:
-    enhanced_image: Imagen con aumento de contraste.
+    Returns:
+        numpy.ndarray: Imagen con aumento de contraste.
     """
     # Primera etapa del algoritmo: Obtener escalas de brillo y oscuridad
     scales = [cv2.getStructuringElement(cv2.MORPH_RECT, (2*i+1, 2*i+1)) for i in range(1, n_iterations+1)]
@@ -40,15 +47,15 @@ def MMC(image, n_iterations):
 def CLAHE(image, clip_limit=2.0, tile_grid_size=(8, 8)):
     """
     Función que implementa el algoritmo de Mejora de Contraste Adaptativo
-    basado en Histogramas Limitados (CLAHE)
+    basado en Histogramas Limitados (CLAHE).
 
-    Parámetros:
-    image: Imagen de entrada en escala de grises.
-    clip_limit: Límite de recorte para el histograma.
-    tile_grid_size: Tamaño de la cuadrícula para la ecualización.
+    Args:
+        image (numpy.ndarray): Imagen de entrada en escala de grises.
+        clip_limit (float): Límite de recorte para el histograma.
+        tile_grid_size (tuple): Tamaño de la cuadrícula para la ecualización.
 
-    Retorna:
-    enhanced_image: Imagen con aumento de contraste.
+    Returns:
+        numpy.ndarray: Imagen con aumento de contraste.
     """
     clahe = cv2.createCLAHE(clipLimit=clip_limit, tileGridSize=tile_grid_size)
     enhanced_image = clahe.apply(image)
@@ -57,62 +64,56 @@ def CLAHE(image, clip_limit=2.0, tile_grid_size=(8, 8)):
 
 def HE(image):
     """
-    Función que implementa el algoritmo de Ecualización de Histograma (HE)
+    Función que implementa el algoritmo de Ecualización de Histograma (HE).
 
-    Parámetros:
-    image: Imagen de entrada en escala de grises.
+    Args:
+        image (numpy.ndarray): Imagen de entrada en escala de grises.
 
-    Retorna:
-    enhanced_image: Imagen con aumento de contraste.
+    Returns:
+        numpy.ndarray: Imagen con aumento de contraste.
     """
     enhanced_image = cv2.equalizeHist(image)
-    
     return enhanced_image
-
-# Eliminar pixeles calientes: radio de pixel = 2.0; Threshold = 50
 
 def remove_hot_pixels(image):
     """
     Elimina los pixeles calientes de una imagen EL.
 
-    Parámetros:
-        image: La imagen EL de entrada.
-        radius: Radio del pixel caliente.
-        threshold: Umbral para la eliminación de pixeles calientes.
+    Args:
+        image (numpy.ndarray): La imagen EL de entrada.
+        radius (float): Radio del pixel caliente.
+        threshold (int): Umbral para la eliminación de pixeles calientes.
 
-    Devuelve:
-        La imagen sin pixeles calientes.
+    Returns:
+        numpy.ndarray: La imagen sin pixeles calientes.
     """
-    radius=2.0
-    threshold=100
+    radius = 2.0
+    threshold = 100
     ret, mask = cv2.threshold(image, 180, 255, cv2.THRESH_BINARY)
     image = cv2.inpaint(image, mask, 2, cv2.INPAINT_TELEA)
-
     return image
 
 # Eliminación de artefactos, empleando algoritmos de imgProcessor
 # Disponible en https://github.com/radjkarl/imgProcessor
-
 def medianThreshold(img, threshold=0.1, size=3, condition='>', copy=True):
-    
-    from scipy.ndimage import median_filter
-    """ Realiza un filtro de mediana y compara los valores de la imagen original con los de la imagen filtrada.
+    """
+    Realiza un filtro de mediana y compara los valores de la imagen original con los de la imagen filtrada.
     Si la diferencia es mayor que el umbral, se reemplaza el valor de la imagen original por el valor de la imagen filtrada.
-    Parámetros:
-    img: Imagen de entrada.
-    threshold: Umbral para la comparación de los valores de la imagen original con los de la imagen filtrada.
-    size: Tamaño de la ventana del filtro de mediana.
-    condition: Condición para la comparación de los valores de la imagen original con los de la imagen filtrada.
-    copy: Booleano que indica si se debe realizar una copia de la imagen original.
-    Devuelve:
-    img: Imagen con los valores reemplazados.
-    indices: Índices de los valores reemplazados.
+
+    Args:
+        img (numpy.ndarray): Imagen de entrada.
+        threshold (float): Umbral para la comparación de los valores de la imagen original con los de la imagen filtrada.
+        size (int): Tamaño de la ventana del filtro de mediana.
+        condition (str): Condición para la comparación de los valores de la imagen original con los de la imagen filtrada.
+        copy (bool): Booleano que indica si se debe realizar una copia de la imagen original.
+
+    Returns:
+        tuple: Imagen con los valores reemplazados y los índices de los valores reemplazados.
     """
     indices = None
     if threshold > 0:
         blur = np.asfarray(median_filter(img, size=size))
         with np.errstate(divide='ignore', invalid='ignore', over='ignore'):
-
             if condition == '>':
                 indices = abs((img - blur) / blur) > threshold
             else:
@@ -125,44 +126,45 @@ def medianThreshold(img, threshold=0.1, size=3, condition='>', copy=True):
     return img, indices
 
 def correctArtefacts(image, threshold):
-    """ Aplica un umbrak a una imagen para corregir artefactos, reemplazando valores más alla de un umbral
-    Parámetros:
-    image: Imagen de entrada.
-    threshold: Umbral para la comparación de los valores de la imagen original con los de la imagen filtrada.
-    Devuelve:
-    image: Imagen corregida.
+    """
+    Aplica un umbral a una imagen para corregir artefactos, reemplazando valores más allá de un umbral.
+
+    Args:
+        image (numpy.ndarray): Imagen de entrada.
+        threshold (float): Umbral para la comparación de los valores de la imagen original con los de la imagen filtrada.
+
+    Returns:
+        numpy.ndarray: Imagen corregida.
     """
     image = np.nan_to_num(image)
     medianThreshold(image, threshold, copy=False)
     return image
 
-def SubtractBG(imageEL, inageBG):
+def SubtractBG(imageEL, imageBG):
     """
     Resta una imagen de fondo a una imagen de EL.
 
-    Parámetros:
-        imageEL: Imagen de EL.
-        imageBG: Imagen de fondo.
+    Args:
+        imageEL (numpy.ndarray): Imagen de EL.
+        imageBG (numpy.ndarray): Imagen de fondo.
 
-    Devuelve:
-        La imagen EL con la imagen de fondo restada.
+    Returns:
+        numpy.ndarray: La imagen EL con la imagen de fondo restada.
     """
-    imageEL = cv2.subtract(imageEL, inageBG)
-
+    imageEL = cv2.subtract(imageEL, imageBG)
     return imageEL
 
 def get_mean_max_min_image(dataset):
     """
     Obtiene la imagen promedio, máxima y mínima de un dataset de imágenes.
 
-    Parámetros:
-        dataset: Lista de imágenes.
+    Args:
+        dataset (list): Lista de imágenes.
 
-    Devuelve:
-        La imagen promedio, máxima y mínima.
+    Returns:
+        tuple: La imagen promedio, máxima y mínima.
     """
     mean_image = np.mean(dataset, axis=0)
     max_image = np.max(dataset, axis=0)
     min_image = np.min(dataset, axis=0)
-
     return mean_image, max_image, min_image
