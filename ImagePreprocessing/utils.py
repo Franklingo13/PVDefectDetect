a7cb83092a2a86c921a2c71b115e70a3c6db0624
Franklin Gómez <franklingo13@gmail.com>
2024-09-23 19:06:02 -0500

    Se actualiza la descripción de funciones y se elimina ficheros.

--- ImagePreprocessing/utils.py
+++ ImagePreprocessing/utils.py
@@ -8,22 +8,16 @@ import cv2
 import matplotlib.patches as patches
 from matplotlib import pyplot as plt
 
-# Función para la lectura de imágenes
 def read_images(directory, allow_color_images=False):
     """
-    Lee todas las imágenes en un directorio
+    Lee todas las imágenes en un directorio.
 
-    Parámetros
+    Args:
+        directory (str): Directorio con las imágenes.
+        allow_color_images (bool): Permite el uso de imágenes a color.
 
-    directory: str
-        Directorio con las imágenes
-    allow_color_images: bool
-        Permite el uso de imágenes a color
-
-    Retorna
-
-    images: list
-        Lista con las imágenes
+    Returns:
+        list: Lista con las imágenes.
     """
     # Leer todas las imágenes en el directorio
     images = []
@@ -39,11 +33,11 @@ def read_folder_path(folder_path):
     """
     Función que lee una ruta de una carpeta y la reescribe de forma que sea legible para el sistema operativo.
 
-    Parámetros:
-    folder_path: Ruta de la carpeta.
+    Args:
+        folder_path (str): Ruta de la carpeta.
 
-    Retorna:
-    folder_path: Ruta de la carpeta legible para el sistema operativo.
+    Returns:
+        str: Ruta de la carpeta legible para el sistema operativo.
     """
     folder_path = folder_path.replace("\\", "/")
     if folder_path[-1] != "/":
@@ -54,13 +48,13 @@ def mostrar_imagen_con_roi(imagen, roi, titulo, esquina_superior_izquierda, anch
     """
     Muestra una imagen con una zona ampliada o ROI.
 
-    Parámetros:
-        imagen: La imagen a mostrar.
-        roi: La zona ampliada (ROI).
-        titulo: El título de la ventana.
-        esquina_superior_izquierda: Una tupla (x, y) que representa las coordenadas de la esquina superior izquierda de la ROI.
-        ancho: El ancho de la ROI.
-        alto: El alto de la ROI.
+    Args:
+        imagen (numpy.ndarray): La imagen a mostrar.
+        roi (numpy.ndarray): La zona ampliada (ROI).
+        titulo (str): El título de la ventana.
+        esquina_superior_izquierda (tuple): Una tupla (x, y) que representa las coordenadas de la esquina superior izquierda de la ROI.
+        ancho (int): El ancho de la ROI.
+        alto (int): El alto de la ROI.
     """
     # Crear una figura de Matplotlib
     fig, axes = plt.subplots(1, 2, figsize=(10, 5))
@@ -85,33 +79,16 @@ def mostrar_imagen_con_roi(imagen, roi, titulo, esquina_superior_izquierda, anch
     # Mostrar la figura
     plt.show()
 
-# Función para guardar una imagen en un directorio
 def save_image(image, directory, filename):
     """
-    Guarda una imagen en un directorio
+    Guarda una imagen en un directorio.
 
-    Parámetros
-
-    image: np.array
-        Imagen a guardar
-    directory: str
-        Directorio donde se guardará la imagen
-    filename: str
-        Nombre de la imagen
+    Args:
+        image (numpy.ndarray): Imagen a guardar.
+        directory (str): Directorio donde se guardará la imagen.
+        filename (str): Nombre de la imagen.
     """
     # Verificar si el directorio existe
     if not path.exists(directory):
         os.makedirs(directory)
     cv2.imwrite(path.join(directory, filename), image)
-
-"""# Almacena el dataset en el output_noBG_path con nombres numerados
-        filename = f"EL_noBG_{i}.jpg"
-        output_filepath = os.path.join(output_noBG_path, filename)
-
-        # Convierte la matriz de imagen a tipo uint8 antes de guardarla
-        dataset_noBG_uint8 = cv2.normalize(dataset_noBG[i], None, 0, 255, cv2.NORM_MINMAX).astype('uint8')
-
-        cv2.imwrite(output_filepath, dataset_noBG_uint8)
-        print(f"Imagen {i} guardada como {filename}")
-        
-        """
